40373bbf6859243bf80a00eec54f5a566212de71
dracangelo <kelvinmbuguaw@gmail.com>
2022-07-30 10:47:06 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>Game of life</h1>
+        <Grid
+          
+        />
+        <h2>Generation: {this.state.generation}</h2>
+      </div>
+    )
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Main/>, document.getElementById('root'));
